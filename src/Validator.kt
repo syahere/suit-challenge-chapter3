fd4b2bcddea6a28e5e1b9e2213b2cf2f9421c635
syahere <syaishere1234@gmail.com>
2021-09-15 12:42:25 +0700

    membenarkan error fungsi validasi

--- src/Validator.kt
+++ src/Validator.kt
@@ -4,9 +4,6 @@ import elements.parent.Suit
 open class Validator {
     open fun validasi(input:String) : Boolean {
         return if (input!= "batu" && input!= "kertas" && input != "gunting"){
-            println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            println("Pilihan yang anda masukkan salah, silahkan input ulang")
-            print("")
             false
         } else {
             true
